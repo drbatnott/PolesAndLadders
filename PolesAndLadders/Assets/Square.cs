e33a730f026efeae205963edb4bc817b0e47879b
Peter Blanchfield <peter@ibitsoftware.org>
2020-02-16 21:59:51 +0000

    Square Tests started
    Tests for correct output when a value in or out of range added.
    Test passed
    Code refactered
    regression tests pass

--- PolesAndLadders/Assets/Square.cs
+++ PolesAndLadders/Assets/Square.cs
@@ -4,8 +4,12 @@ using System.Collections;
 public class Square : MonoBehaviour {
 	public int number;
 	// Use this for initialization
+
 	void Start () {
-	
+		if (number < 0 || number > 80) {
+			number = -1;
+		}
+		Debug.Log (number);
 	}
 	
 	// Update is called once per frame
---
+++ PolesAndLadders/Assets/SquareSpawner.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SquareSpawner : MonoBehaviour {
+
+	public GameObject protoSquare;
+	GameObject [] square1;
+
+	// Use this for initialization
+	void Start () {
+		square1 = new GameObject[81];
+		for (int i = 0; i < 78; i++) {
+			square1 [i] = GameObject.Instantiate (protoSquare);
+			square1 [i].GetComponent<Square> ().number = i;
+		}
+		square1[79] = GameObject.Instantiate (protoSquare);
+		square1[79].GetComponent<Square> ().number = -2;
+		square1[80] = GameObject.Instantiate (protoSquare);
+		square1[80].GetComponent<Square> ().number = 85;
+		square1[50] = GameObject.Instantiate (protoSquare);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
